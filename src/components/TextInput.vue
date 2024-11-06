4c0a74975468183131a3399e7b325c7c677eb6eb
Sebastián Vašš <sebastian9vass@gmail.com>
2024-11-06 21:15:09 +0100

    Prepare input for API key

--- src/components/TextInput.vue
+++ src/components/TextInput.vue
@@ -7,12 +7,12 @@ import { Label } from 'radix-vue'
     <Label
       class="text-[15px] font-semibold leading-[35px]"
       for="firstName"
-    > First name </Label>
+    > Enter API key</Label>
     <input
-      id="firstName"
-      class="bg-blackA5 shadow-blackA9 inline-flex h-[35px] w-[200px] appearance-none items-center justify-center rounded-[4px] px-[10px] text-[15px] leading-none shadow-[0_0_0_1px] outline-none focus:shadow-[0_0_0_2px_black] selection:color-white selection:bg-blackA9"
+      id="api-key"
+      class="bg-blackA5 shadow-blackA9 inline-flex h-[35px] w-[320px] appearance-none items-center justify-center rounded-[4px] px-[10px] text-[15px] leading-none shadow-[0_0_0_1px] outline-none focus:shadow-[0_0_0_2px_black] selection:color-white selection:bg-blackA9"
       type="text"
-      value="Pedro Duarte"
+      value="aaabbbccc-1234-abcd-5678-eee222ccc"
     >
   </div>
 </template>
